7f49ef938e82d91bd9a5194616cf723ff9fae7c3
Aashu <aashukushwah53678@gmail.com>
2025-04-08 01:08:35 +0530

    Make selection screen responsive

--- src/components/LevelSelection.tsx
+++ src/components/LevelSelection.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from "next/navigation";
 import "../styles/level-selection.css";
 import { useState } from "react";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function LevelSelection() {
   const router = useRouter();
@@ -9,16 +11,46 @@ export default function LevelSelection() {
 
   return (
     <div className="popup-overlay">
+      <nav className="mobile-nav">
+        <FaRegUserCircle size={32} color="black" />
+        <div>
+          <IoSettingsOutline size={32} color="black" />
+        </div>
+      </nav>
       <div className="popup-content">
         <span className="close-icon" onClick={() => router.push("/")}>
           ×
         </span>
-        <h2>Choose your preferred level</h2>
-        <button  className={level==='easy' ? "selected-level":""} onClick={() => setLevel("easy")}>EASY</button>
-        <button className={level==='medium' ? "selected-level":""} onClick={() => setLevel("medium")}>MEDIUM</button>
-        <button className={level==='hard' ? "selected-level":""}  onClick={() => setLevel("hard")}>HARD</button>
-        <button  className={level==='expert' ? "selected-level":""}onClick={() => setLevel("expert")}>EXPERT</button>
-        <button className="start-button" disabled={!level} onClick={() => router.push(`/game/play?level=${level}`)}>
+        <h2 className="popup-title">Choose your preferred level</h2>
+        <button
+          className={level === "easy" ? "selected-level" : ""}
+          onClick={() => setLevel("easy")}
+        >
+          EASY
+        </button>
+        <button
+          className={level === "medium" ? "selected-level" : ""}
+          onClick={() => setLevel("medium")}
+        >
+          MEDIUM
+        </button>
+        <button
+          className={level === "hard" ? "selected-level" : ""}
+          onClick={() => setLevel("hard")}
+        >
+          HARD
+        </button>
+        <button
+          className={level === "expert" ? "selected-level" : ""}
+          onClick={() => setLevel("expert")}
+        >
+          EXPERT
+        </button>
+        <button
+          className="start-button"
+          disabled={!level}
+          onClick={() => router.push(`/game/play?level=${level}`)}
+        >
           START
         </button>
       </div>
